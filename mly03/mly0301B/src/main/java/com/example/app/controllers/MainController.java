d434e0ab444a531d08b85b79281e475c01762340
MartaLopezY <144225848+MartaLopezY@users.noreply.github.com>
2023-09-30 18:18:36 +0200

    Add files via upload

---
+++ mly03/mly0301B/src/main/java/com/example/app/controllers/MainController.java
@@ -0,0 +1,26 @@
+package com.example.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping({"", "/", "/home", "/index"})
+    public  String showMain() {
+
+        return "indexView";
+    }
+    @GetMapping("/contacta")
+    public  String showContacta() {
+
+        return "contactaView";
+    }
+    @GetMapping("/producto")
+    public  String showProducto() {
+
+        return "productoView";
+    }
+
+
+}
