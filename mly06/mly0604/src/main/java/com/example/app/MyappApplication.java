1a1a0cce314d9d15485e310cb005732ed7cf608d
MartaLopezY <144225848+MartaLopezY@users.noreply.github.com>
2023-11-12 18:39:54 +0100

    Update MyappApplication.java

--- mly06/mly0604/src/main/java/com/example/app/MyappApplication.java
+++ mly06/mly0604/src/main/java/com/example/app/MyappApplication.java
@@ -9,5 +9,16 @@ public class MyappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyappApplication.class, args);
 	}
+@Bean
+	CommandLineRunner initData(ProductoService productoService) {
+		return args -> {
 
+			productoService.agregar(
+					new Producto(1L, "mesa", true, NORMAL, 100d));
+			productoService.agregar(
+					new Producto(2L, "silla", false, NORMAL, 50d));
+			productoService.agregar(
+					new Producto(3L, "estanteria", true, REDUCIDO, 70d));
+		};
+	}
 }
