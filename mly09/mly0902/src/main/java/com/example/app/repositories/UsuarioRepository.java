08040fec5247f903c9240b3431bad0da30084f5e
MartaLopezY <144225848+MartaLopezY@users.noreply.github.com>
2024-01-25 08:57:08 +0100

    Ejercicio 2 tema 9

---
+++ mly09/mly0902/src/main/java/com/example/app/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.app.repositories;
+
+import com.example.app.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository  extends JpaRepository<Usuario, Long> {
+    Usuario findByNombre(String nombre);
+
+}
