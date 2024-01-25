417f1e07e31da136b19eb8382d4da49dc3b2770e
MartaLopezY <144225848+MartaLopezY@users.noreply.github.com>
2024-01-25 10:39:01 +0100

    Delete mly09/mly0902 directory

--- mly09/mly0902/src/main/java/com/example/app/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.repositories;
-
-import com.example.app.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository  extends JpaRepository<Usuario, Long> {
-    Usuario findByNombre(String nombre);
-
-}
